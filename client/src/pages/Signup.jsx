63bb5ba6968a442149ac86ffd44caf0caea0e744
daniyal husain <daniyalhusain10@gmail.com>
2025-07-18 21:06:28 +0500

    Update Signup.jsx

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -14,7 +14,7 @@ export const Signup = ()=>{
       const navigate = useNavigate()
 
    const onSubmit = async (data) =>{
-    let res = await fetch('http://localhost:5000/signup', 
+    let res = await fetch('https://login-system-production.up.railway.app/signup', 
        {method: "POST" ,
     body: JSON.stringify(data),
     headers: {'Content-Type': 'application/json'},
