9eac1b568f3bf8e28c478f57362a724ff256bfd2
daniyal husain <daniyalhusain10@gmail.com>
2025-07-18 23:08:42 +0500

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -15,7 +15,7 @@ export const Login = ()=>{
  const Navigate = useNavigate()
  //caling api or recieving data for log in
    const onSubmit = async (data) =>{
-    let res = await fetch('https://login-system-production.up.railway.app/login',
+    let res = await fetch('https://login-system-production.up.railway.app/api/login',
     {method: "POST" ,
     body: JSON.stringify(data),
     headers: {'Content-Type': 'application/json'},
